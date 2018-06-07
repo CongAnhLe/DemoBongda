f556e98fdd59126223a500dd93b0fac1ad71b966
ngoduchuy1996 <40018738+ngoduchuy1996@users.noreply.github.com>
2018-06-07 14:55:14 +0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.anhcong.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //Tọadasiodasiodhioashdioas
+        //ÀIiajsfijasifjasifjio
+        //Lê Anh CÔng Demo - 01
+    }
+}
